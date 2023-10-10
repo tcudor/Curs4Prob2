81f8d49a6c5a7fa9a2e6c2cda3352e313616fe62
tudorg <tudorg@indsoft.ro>
2023-10-10 15:52:00 +0300

    Add project files.

---
+++ ArrayHelper.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercises
+{
+    public static class ArrayHelper
+    {
+        public static int Min(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Array must have at least 1 element",
+                    nameof(array));
+            }
+
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static int Max(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Array must have at least 1 element",
+                    nameof(array));
+            }
+
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static int Sum(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static double Average(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Array must have at least 1 element",
+                    nameof(array));
+            }
+
+            int sum = Sum(array);
+            double average = (double)sum / array.Length;
+            return average;
+        }
+
+        public static int[] ReverseArray(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int[] reversedArray = new int[array.Length];
+            for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+            {
+                reversedArray[j] = array[i];
+            }
+
+            return reversedArray;
+        }
+
+        public static int Frequency(int[] array, int element)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int frequency = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                {
+                    frequency++;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ArrayExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = ConsoleHelper.ReadNumber("The size of the array = ", 3, 0);
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = ConsoleHelper.ReadNumber($"element[{i}] = ", 3, 0);
+                array[i] = element;
+            }
+
+            ConsoleHelper.PrintArray("Array=", array);
+
+            int minValue = ArrayHelper.Min(array);
+            Console.WriteLine($"Min = {minValue}");
+
+            int maxValue = ArrayHelper.Max(array);
+            Console.WriteLine($"Max = {maxValue}");
+
+            int sum = ArrayHelper.Sum(array);
+            Console.WriteLine($"Sum = {sum}");
+
+            double average = ArrayHelper.Average(array);
+            Console.WriteLine($"Average = {average}");
+
+            int[] reversedArray = ArrayHelper.ReverseArray(array);
+            Console.WriteLine($"Reversed Array = [{string.Join(", ", reversedArray)}]");
+
+            int elementToFind = ConsoleHelper.ReadNumber("Element to find = ", 3, 0);
+            int frequency = ArrayHelper.Frequency(array, elementToFind);
+            Console.WriteLine($"Frequency of {elementToFind} = {frequency}");
+        }
+    }
+}
